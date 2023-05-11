1630dbe7bf64c1539a656b0900dfcb89de4362fb
vikas gupta <vikasaggrawal700@gmail.com>
2023-05-11 11:30:21 +0530

    Basic routes setting

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const port = 5000;
 
+const placesRoutes = require("./routes/places-routes");
+const userRoutes = require("./routes/user-routes");
+
 const app = express();
+
+app.use("/api/places", placesRoutes);
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Working");
