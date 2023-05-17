ab2507284b515a94fc5ea128424742e2f41ab9f2
vikas gupta <vikasaggrawal700@gmail.com>
2023-05-17 21:21:42 +0530

    Fixes bug of jwt token decoding

--- app.js
+++ app.js
@@ -32,7 +32,10 @@ app.use("/upload/images", express.static(path.join("upload", "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Authorization"
+  );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH");
   next();
 });
--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -2,15 +2,20 @@ const HttpError = require("../models/http-error");
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return next();
+  }
   try {
-    const token = req.header.auth.split(" ")[1];
+    const token = req.headers.authorization.split(" ")[1];
+    console.log(token);
     if (!token) {
-      throw new Error("Authentical Failed!!");
+      throw new Error("Authentication Failed!!");
     }
-    const tokenValue = jwt.verify(token, donttrytohackmykeyforwrongwork);
-    req.userData = { userId: tokenValue.userId };
+    const tokenValue = jwt.verify(token, "donttrytohackmykeyforwrongwork");
+    // req.userData = { userId: tokenValue.userId };
     next();
   } catch (err) {
-    return next(new HttpError("Authentical Failed!!", 401));
+    console.log(err);
+    return next(new HttpError("Authentication Failed!!", 401));
   }
 };
