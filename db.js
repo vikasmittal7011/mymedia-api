820dbdba78092415b5a112d96e811ed61122d387
vikas gupta <vikasaggrawal700@gmail.com>
2023-05-19 15:57:32 +0530

    modify mongo connection string

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectToMongoose = async () => {
   // mongoose.connect("mongodb://127.0.0.1/myMedia");
   mongoose.connect(
-    `mongodb+srv://${process.env.Db_User}:${process.env.Db_Password}@cluster0.lqtjft2.mongodb.net/${process.env.Db_Name}`
+    `mongodb+srv://${process.env.Db_User}:${process.env.Db_Password}@cluster0.lqtjft2.mongodb.net/${process.env.Db_Name}?retryWrites=true&w=majority`
   );
 };
 
